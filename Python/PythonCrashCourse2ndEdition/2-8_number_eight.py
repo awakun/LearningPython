79a95311e02f739a4182a15e760b15c5e746bd3e
Dave Kaylor <worgenftw@gmail.com>
2021-01-22 19:56:56 -0800

    Added 2-8 and 2-9 exercises

---
+++ Python/PythonCrashCourse2ndEdition/2-8_number_eight.py
@@ -0,0 +1,6 @@
+print(4+4)
+print(15-7)
+print(2*4)
+# print(16/2) Technically I believe this solves the exercise, but fixing anyways because it doesn't match the others.
+# Learned rstrip() can be used like PowerShell's trim()
+print(f"{16/2}".rstrip('.0'))
