9184043f0797addc91d1396420577905818d95ec
Dave Kaylor <worgenftw@gmail.com>
2021-01-23 20:31:44 -0800

    ch4 exer 3-8

---
+++ Python/PythonCrashCourse2ndEdition/4-4_one_million.py
@@ -0,0 +1,8 @@
+numbers = list(range(1, 1_000_001))
+for number in numbers:
+    print(number)
+
+# This is exercise 4-5:
+print(min(numbers))
+print(max(numbers))
+print(sum(numbers))
---
+++ Python/PythonCrashCourse2ndEdition/4-8_cubes.py
@@ -0,0 +1,4 @@
+# This may not be the cleanest, seeing if it works though
+# oh also this solves 4-9 (that was an accident!)
+for number in [value ** 3 for value in range(1, 11)]:
+    print(number)
